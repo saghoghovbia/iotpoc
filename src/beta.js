e93271e31c046e34972848667af1e35be55764f9
saghoghovbia <62894560+saghoghovbia@users.noreply.github.com>
2022-09-19 12:10:13 -0500

    Polish

--- src/beta.js
+++ src/beta.js
@@ -6,7 +6,6 @@ function Beta(){
         <div className = 'hero'>
              <div className = 'navbar'>
                     <img src='https://freepngimg.com/thumb/chibi/12-2-chibi-download-png.png' alt='MAL Logo' className= 'logo'></img>
-                    {/* <button type='button'>Yerr</button> */}
                 </div>
             <div className = 'content'>
                 <div className = 'text'>
@@ -14,15 +13,9 @@ function Beta(){
                 <h1>Active <br></br>
                 Return message</h1>
                 </div>
-                {/* <button type="button">Aye</button> */}
                 </div>
                 <div className='sidebar'>
                     <img src='https://images6.fanpop.com/image/photos/35800000/Killua-killua-zoldyck-2011-35806939-500-281.gif' alt='HxH' className='menu'></img>
-            
-                <div className='social-link'>
-                    <a href='https://myanimelist.net' target='_blank'><img src='http://pbs.twimg.com/profile_images/1190380284295950339/Py6XnxvH_400x400.jpg' alt='MAL Logo'></img></a>
-                </div>
-                
             </div>
         </div>
     )
