376518b357f76bbc3645995ef4c8a7c4f51b8c97
saghoghovbia <62894560+saghoghovbia@users.noreply.github.com>
2022-09-19 14:51:15 -0500

    Back end code added

---
+++ Backend/connect.py
@@ -0,0 +1,23 @@
+import socket
+
+s = socket.socket()
+print ('Socket successfully created')
+
+port = 12346
+
+s.bind(('192.168.1.182', port))
+print ('socket binded to %s' %(port) )
+
+s.listen(5)
+print ('socket is listening')
+
+
+def rp(s):
+    c, arr = s.accept()
+    data = c.recv(1024).decode("utf-8")
+    print(str(data))
+    c.close() 
+
+while True:
+    rp(s)
+
